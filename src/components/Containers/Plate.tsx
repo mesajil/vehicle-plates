04618d23e389cdbb389056434ca7587c4dae9f70
Luis Mesajil <lhmesajil@gmail.com>
2024-02-08 19:46:23 -0500

    feat(Plate): Finish Plate styles

--- src/components/Containers/Plate.tsx
+++ src/components/Containers/Plate.tsx
@@ -4,10 +4,17 @@ interface PlateProps {
   padding?: string
   border?: string
   margin?: string
+  backgroundColor?: string
 }
 
 export const Plate = styled.div<PlateProps>`
-  padding: ${props => props.padding || '0'};
-  border: ${props => props.border || 'none'};
+  padding: ${props => props.padding || '5px 6px 2px 6px'};
+  border: ${props => props.border || '2px ridge gray'};
   margin: ${props => props.margin || '0'};
+  background-color: ${props => props.backgroundColor || 'red'};
+  border-radius: 3px;
+  font-family: 'LICENSE PLATE USA', sans-serif;
+  font-weight: bold;
+  text-shadow: 0px 1px 2px #919191;
+  box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.5);
 `
